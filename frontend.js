2302256281f6e7a80c65a214c3f3bf30f6166971
Nikolay Rudenko <r.nikolay.e@gmail.com>
2018-03-13 13:05:21 +0200

    Implement private_sign sample

--- frontend.js
+++ frontend.js
@@ -1,101 +1,59 @@
 "use strict";
 
-var web3AuthFrontEnd = require('web3-auth/frontend.js')
+var ethUtil = require('ethereumjs-util')
 var $ = require('jquery');
 var Web3 = require('web3');
 
-window.addEventListener('load', function() {
+var signTextPrivate = function (from, text, next) {
+  var msg = ethUtil.bufferToHex(new Buffer(text, 'utf8'));
+  var params = [msg, from];
+  var method = 'personal_sign';
+  return web3
+    .currentProvider
+    .sendAsync({
+      method,
+      params,
+      from
+    }, function (err, result) {
+      if (err) 
+        next(result, err);
+      if (result.error) 
+        next(result, result.error);
+      const msgParams = {
+        data: msg,
+        sig: result.result
+      }
+      next(msgParams, null);
+    });
+};
+
+var authenticate = function (data) {
+  console.log('PERSONAL SIGNED:' + JSON.stringify(data));
+  $.ajax({
+    type: "POST",
+    url: 'http://localhost:8000/api/auth',
+    data: JSON.stringify(data),
+    success: saveToken,
+    dataType: 'json'
+  });
+}
+
+var saveToken = function (result) {
+  console.log(result);
+}
+
+window.addEventListener('load', function () {
 
   window.web3 = new Web3(web3.currentProvider);
 
-  web3.version.getNode(function(error, result) {
-    if(!error) {
-      $("#api").html(web3.version.api);
-      $("#node").html(result);
-      $("#account").html(web3.eth.accounts[0]);
-    }
-    else {
-      console.error(error);
-    }
-  });
-
-  web3.version.getNetwork(function(error, result) {
-    if(!error) {
-      $("#network").html(result);
-    }
-    else {
-      console.error(error);
-    }
-  });
-
-  web3.version.getEthereum(function(error, result) {
-    if(!error) {
-      $("#ethereum").html(result);
-    }
-    else {
-      console.error(error);
-    }
-  });
-
-  web3.net.getPeerCount(function(error, result) {
-    if(!error) {
-      $("#peer-count").html(result);
-    }
-    else {
-      console.error(error);
-    }
-  });
-
-  web3.eth.getGasPrice(function(error, result) {
-    if(!error) {
-      $("#gas-price").html(result.toString(10));
-    }
-    else {
-      console.error(error);
-    }
-  });
-
-  web3.eth.getBlockNumber(function(error, result) {
-    if(!error) {
-      $("#block-number").html(result);
-    }
-    else {
-      console.error(error);
-    }
-  });
-
-  web3.eth.filter('latest', function(error, result) {
-    if (!error) {
-      web3.eth.getBlockNumber(function(error, result) {
-        if(!error) {
-          $("#block-number").html(result);
-        }
-        else {
-          console.error(error);
-        }
-      });
-    }
-    else {
-      console.error(error);
-    }
-  });
-
   $('#button').click(function (event) {
-    web3AuthFrontEnd.signIn();
-  });
+    var text = 'Welcome to the EtherApp!'
+    web3
+      .eth
+      .getAccounts()
+      .then(accounts => {
+        signTextPrivate(accounts[0], text, authenticate);
+      });
 
-  $('#who').click(function (event) {
-    $.ajax({
-      method: 'GET',
-      url: 'http://localhost:3000/who',
-      dataType: 'json',
-      success: function (data, textStatus, jqXHR) {
-        alert(data.account);
-      },
-      error: function (jqXHR, textStatus, errorThrown) {
-        console.log('error');
-      }
-    });
   });
-
-});
+})
